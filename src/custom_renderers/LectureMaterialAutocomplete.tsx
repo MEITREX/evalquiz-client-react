2b3932b15b9ab9df7134eea4dc5dd54798a7e923
st170001 <st170001@stud.uni-stuttgart.de>
2023-09-18 01:23:45 +0200

    Lecture material selection shows (name, hash) pairs

--- src/custom_renderers/LectureMaterialAutocomplete.tsx
+++ src/custom_renderers/LectureMaterialAutocomplete.tsx
@@ -1,30 +1,56 @@
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 interface Props {
   value: string;
-  updateValue: (newValue: string) => void;
+  updateValue: (newHash: string) => void;
 }
 
-const test = ["9384304", "sjdkflsjdf"];
+const test = [
+  { name: "hello", hash: "9384304" },
+  { name: "wow", hash: "sjdkflsjdf" },
+];
 
 export default function LectureMaterialAutocomplete({
   value,
   updateValue,
 }: Props) {
+  const referencedHashs = test.map((valueNamePair) => ({
+    displayText: valueNamePair.name.concat(", ", valueNamePair.hash),
+    hash: valueNamePair.hash,
+  }));
+
+  const displayTexts = referencedHashs.map(
+    (displayTextHashPair) => displayTextHashPair.displayText
+  );
+
+  const [displayText, setDisplayText] = useState(displayTexts[0]);
+
+  const displayTextToHash = (displayText: string) => {
+    let hash = null;
+    referencedHashs.forEach((referencedHash) => {
+      if (referencedHash.displayText === displayText) {
+        hash = referencedHash.hash;
+      }
+    });
+    return hash;
+  };
+
   return (
     <Autocomplete
       disablePortal
-      value={value}
-      onChange={(_, newValue) => {
-        if (newValue === null || newValue === undefined) {
+      value={displayText}
+      onChange={(_, newDisplayText) => {
+        if (newDisplayText === null || newDisplayText === undefined) {
           updateValue(value);
         } else {
-          updateValue(newValue);
+          setDisplayText(newDisplayText);
+          updateValue(displayTextToHash(newDisplayText) ?? value);
         }
       }}
       id="combo-box-demo"
-      options={test}
+      options={displayTexts}
       sx={{ width: 300 }}
       renderInput={(params) => (
         <TextField {...params} label="Lecture Material" />
